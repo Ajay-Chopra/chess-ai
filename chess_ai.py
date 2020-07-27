f9d5b0133975ab7a32c581550d5277d3a2ce8691
Ajay Chopra <aechopra22@gmail.com>
2020-07-27 15:22:44 -0500

    added updated board evaluation function

--- chess_ai.py
+++ chess_ai.py
@@ -1,71 +1,164 @@
 import chess
 import chess.svg
-import piece_square_tables
+import piece_square_tables as pst
 import sys
+
+def init_board_value(board):
+    '''
+    Evaluates value of the starting board using piece squares tables and
+    weights assigned to each piece type
+    '''
+    global board_value
+    # First, calculate total material on board
+    white_pawns = len(board.pieces(chess.PAWN, chess.WHITE)) # Number of white pawns on board
+    black_pawns = len(board.pieces(chess.PAWN, chess.BLACK))
+    white_knights = len(board.pieces(chess.KNIGHT, chess.WHITE))
+    black_knights = len(board.pieces(chess.KNIGHT, chess.BLACK))
+    white_bishops = len(board.pieces(chess.BISHOP, chess.WHITE))
+    black_bishops = len(board.pieces(chess.BISHOP, chess.BLACK))
+    white_rooks = len(board.pieces(chess.ROOK, chess.WHITE))
+    black_rooks = len(board.pieces(chess.ROOK, chess.BLACK))
+    white_queen = len(board.pieces(chess.QUEEN, chess.WHITE))
+    black_queen = len(board.pieces(chess.QUEEN, chess.BLACK))
+    white_king = len(board.pieces(chess.KING, chess.WHITE))
+    black_king = len(board.pieces(chess.KING, chess.BLACK))
+
+    # Calc material value using weights given here: https://www.chessprogramming.org/Simplified_Evaluation_Function
+    total_material = (100*(white_pawns - black_pawns) + 320*(white_knights - black_knights)
+                    + 330*(white_bishops - black_bishops) + 500*(white_rooks - black_rooks)
+                    + 900*(white_queen - black_queen))
+
+    # Calculate the positional values of the pieces using piece-square tables
+    pawn_table = pst.pawn_ps
+    knight_table = pst.knight_ps
+    bishop_table = pst.bishop_ps
+    rook_table = pst.rook_ps
+    queen_table = pst.queen_ps
+
+    pawn_pos_val = (sum(pawn_table[i] for i in board.pieces(chess.PAWN, chess.WHITE))
+                   + sum(-pawn_table[chess.square_mirror(i)] for i in board.pieces(chess.PAWN, chess.BLACK)))
+
+    knight_pos_val = (sum(knight_table[i] for i in board.pieces(chess.KNIGHT, chess.WHITE))
+                   + sum(-knight_table[chess.square_mirror(i)] for i in board.pieces(chess.KNIGHT, chess.BLACK)))
+
+    bishop_pos_val = (sum(bishop_table[i] for i in board.pieces(chess.BISHOP, chess.WHITE))
+                   + sum(-bishop_table[chess.square_mirror(i)] for i in board.pieces(chess.BISHOP, chess.BLACK)))
+
+    rook_pos_val = (sum(rook_table[i] for i in board.pieces(chess.ROOK, chess.WHITE))
+                   + sum(-rook_table[chess.square_mirror(i)] for i in board.pieces(chess.ROOK, chess.BLACK)))
+
+    queen_pos_val = (sum(queen_table[i] for i in board.pieces(chess.QUEEN, chess.WHITE))
+                   + sum(-queen_table[chess.square_mirror(i)] for i in board.pieces(chess.QUEEN, chess.BLACK)))
+
+    board_value = (total_material + pawn_pos_val + knight_pos_val + bishop_pos_val
+                      + rook_pos_val + queen_pos_val)
+
+    return board_value
+
 
 def evaluate_board(board):
     '''
-    Given a board, evaluate the board state based on piece-square tables
+    Evaluate any given board
+    First check for checkmate or stalement. If neither condition is true
+    return the value of the board from the current value of the global variable
     '''
     if board.is_checkmate():
         if board.turn:
-            return 9999
-        else:
             return -9999
-    elif board.is_stalemate() or board.is_insufficient_material():
-        return 0
-    else:
-        # First, calculate total material on board
-        white_pawns = len(board.pieces(chess.PAWN, chess.WHITE)) # Number of white pawns on board
-        black_pawns = len(board.pieces(chess.PAWN, chess.BLACK))
-        white_knights = len(board.pieces(chess.KNIGHT, chess.WHITE))
-        black_knights = len(board.pieces(chess.KNIGHT, chess.BLACK))
-        white_bishops = len(board.pieces(chess.BISHOP, chess.WHITE))
-        black_bishops = len(board.pieces(chess.BISHOP, chess.BLACK))
-        white_rooks = len(board.pieces(chess.ROOK, chess.WHITE))
-        black_rooks = len(board.pieces(chess.ROOK, chess.BLACK))
-        white_queen = len(board.pieces(chess.QUEEN, chess.WHITE))
-        black_queen = len(board.pieces(chess.QUEEN, chess.BLACK))
-        white_king = len(board.pieces(chess.KING, chess.WHITE))
-        black_king = len(board.pieces(chess.KING, chess.BLACK))
-
-        # Calc material value using weights given here: https://www.chessprogramming.org/Simplified_Evaluation_Function
-        total_material = (100*(white_pawns - black_pawns) + 320*(white_knights - black_knights)
-                        + 330*(white_bishops - black_bishops) + 500*(white_rooks - black_rooks)
-                        + 900*(white_queen - black_queen))
-
-        # Calculate the positional values of the pieces using piece-square tables
-        pawn_table = piece_square_tables.pawn_ps
-        knight_table = piece_square_tables.knight_ps
-        bishop_table = piece_square_tables.bishop_ps
-        rook_table = piece_square_tables.rook_ps
-        queen_table = piece_square_tables.queen_ps
-
-        pawn_pos_val = (sum(pawn_table[i] for i in board.pieces(chess.PAWN, chess.WHITE))
-                       + sum(-pawn_table[chess.square_mirror(i)] for i in board.pieces(chess.PAWN, chess.BLACK)))
-
-        knight_pos_val = (sum(knight_table[i] for i in board.pieces(chess.KNIGHT, chess.WHITE))
-                       + sum(-knight_table[chess.square_mirror(i)] for i in board.pieces(chess.KNIGHT, chess.BLACK)))
-
-        bishop_pos_val = (sum(bishop_table[i] for i in board.pieces(chess.BISHOP, chess.WHITE))
-                       + sum(-bishop_table[chess.square_mirror(i)] for i in board.pieces(chess.BISHOP, chess.BLACK)))
-
-        rook_pos_val = (sum(rook_table[i] for i in board.pieces(chess.ROOK, chess.WHITE))
-                       + sum(-rook_table[chess.square_mirror(i)] for i in board.pieces(chess.ROOK, chess.BLACK)))
-
-        queen_pos_val = (sum(queen_table[i] for i in board.pieces(chess.QUEEN, chess.WHITE))
-                       + sum(-queen_table[chess.square_mirror(i)] for i in board.pieces(chess.QUEEN, chess.BLACK)))
-
-        total_board_val = (total_material + pawn_pos_val + knight_pos_val + bishop_pos_val
-                          + rook_pos_val + queen_pos_val)
-
-        if board.turn:
-            return total_board_val
         else:
-            return -total_board_val
+            return 9999
+    if board.is_stalemate():
+        return 0
 
+    if board.is_insufficient_material():
+        return 0
 
+    eval = board_value
+    if board.turn:
+        return eval
+    else:
+        return -eval
 
+def update_board_val(piecetypes, tables, piecevalues, move, turn, board):
+    '''
+    Given a move, update the global board_value
+    '''
+    global board_value
+    # Update based on piecesquare tables
+    piece = board.piece_type_at(move.from_square)
+    # Subtract value of square that piece move from
+    if turn:
+        board_value -= tables[piece - 1][move.from_square]
+        # Handle castling
+        if (move.from_square == chess.E1) and (move.to_square == chess.G1):
+            board_value -= tables[3][chess.H1]
+            board_value += tables[3][chess.F1]
+        elif (move.from_square == chess.E1) and (move.to_square == chess.C1):
+            board_value -= tables[3][chess.A1]
+            board_value += tables[3][chess.D1]
+    else:
+        board_value = board_value + tables[piece - 1][move.from_square]
+        # Handle castling
+        if (move.from_square == chess.E8) and (move.to_square == chess.G8):
+            board_value -= tables[3][chess.H8]
+            board_value += tables[3][chess.F8]
+        elif (move.from_square == chess.E8) and (move.to_square == chess.C8):
+            board_value -= tables[3][chess.A8]
+            board_value += tables[3][chess.D8]
+
+    # Add value of square that piece is moving to
+
+    if turn:
+        board_value += tables[piece - 1][move.to_square]
+    else:
+        board_value -= tables[piece - 1][move.to_square]
+
+    # Update material value
+    if move.drop != None:
+        if turn:
+            board_value += piecevalues[move.drop - 1]
+        else:
+            board_value -= piecevalues[move.drop - 1]
+    
+    # Update promotion
+    if move.promotion != None:
+        if turn:
+            board_value += piecevalues[move.promotion - 1] - piecevalues[piece - 1]
+            board_value -= (tables[piece - 1][move.to_square] 
+                + tables[move.promotion - 1][move.to_square])
+        else:
+            board_value -= piecevalues[move.promotion-1] + piecevalues[piece-1]
+            board_value += (tables[piece - 1][move.to_square]
+                - tables[move.promotion - 1][move.to_square])
+    
+    return move
+
+def make_move(board, move):
+    update_board_val(
+        piecetypes = [chess.PAWN, chess.KNIGHT, chess.BISHOP, chess.ROOK, chess.QUEEN, chess.KING],
+        tables = [pst.pawn_ps, pst.knight_ps, pst.bishop_ps, pst.rook_ps, pst.queen_ps, pst.king_middle_ps],
+        piecevalues = [100,320,330,500,900],
+        move = move,
+        turn = board.turn,
+        board = board
+        )
+    
+    board.push(move)
+    return move
+    
+
+def unmake_move(board):
+    move = board.pop()
+    update_board_val(
+        piecetypes = [chess.PAWN, chess.KNIGHT, chess.BISHOP, chess.ROOK, chess.QUEEN, chess.KING],
+        tables = [pst.pawn_ps, pst.knight_ps, pst.bishop_ps, pst.rook_ps, pst.queen_ps, pst.king_middle_ps],
+        piecevalues = [100,320,330,500,900],
+        move = move,
+        turn = not board.turn,
+        board = board
+    )
+    return move
+    
 def minimax(board, depth, alpha, beta, maximizingPlayer):
     '''
     Perform depth first search of possible moves to find the best move
@@ -77,9 +170,9 @@ def minimax(board, depth, alpha, beta, maximizingPlayer):
         max_val = -10000
         best_move = None
         for move in board.legal_moves:
-            board.push(move)
+            make_move(board, move)
             child_val, move_made = minimax(board, depth - 1, alpha, beta, maximizingPlayer = False)
-            board.pop()
+            unmake_move(board)
             if child_val > max_val:
                 max_val = child_val
                 best_move = move
@@ -91,9 +184,9 @@ def minimax(board, depth, alpha, beta, maximizingPlayer):
         min_val = 10000
         best_move = None
         for move in board.legal_moves:
-            board.push(move)
+            make_move(board, move)
             child_val, move_made = minimax(board, depth - 1, alpha, beta, maximizingPlayer = True)
-            board.pop()
+            unmake_move(board)
             if child_val < min_val:
                 min_val = child_val
                 best_move = move
@@ -115,9 +208,9 @@ def quiet_search(board, alpha, beta):
 
     for move in board.legal_moves:
         if board.is_capture(move):
-            board.push(move)
+            make_move(board, move)
             score = -quiet_search(board, -alpha, -beta)
-            board.pop()
+            unmake_move(board)
 
             if (score >= beta):
                 return beta
@@ -141,6 +234,7 @@ def run_game():
     print("For example if you wanted to move from F2 to F3 you would type f2f3")
     print("---------------------------------------------------------------------")
     board = chess.Board()
+    init_board_value(board)
     print(board)
     print("---------------------------------------------------------------------")
     while True:
@@ -166,4 +260,10 @@ def run_game():
                     break
                 else:
                     print("This is not a legal move")
+
 run_game()
+
+
+
+
+
